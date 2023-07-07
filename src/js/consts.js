38e5e25c81c94a9ff33824a05b36b813c1f28835
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-07-07 16:45:07 +0300

    add more words in array

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,3 +1,24 @@
-export const WORDS = ['code', 'frontend', 'programming'];
+export const WORDS = [
+  "code",
+  "sex",
+  "car",
+  "bar",
+  "weather",
+  "game",
+  "sophistication",
+  "experience",
+  "marina",
+  "misha",
+  "bbq",
+  "anya",
+  "lisa",
+  "slava",
+  "dasha",
+  "kristina",
+  "nastya",
+  "vitalick",
+  "frontend",
+  "programming",
+];
 
-export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ'); // массив из букв
+export const KEYBOARD_LETTERS = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ"); // массив из букв
